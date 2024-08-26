a19805c008ff84a985d35292d9ad06cd70cf7030
Bode20029 <jry.bode@gmail.com>
2024-08-26 08:35:48 +0700

    last

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import qrcode
 import tkinter as tk
+from PIL import ImageTk, Image
 
-if __name__ == '_main__':
-    window = tk.TK()
+def generate_qr_code():
+    info = "https://www.youtube.com/@JirayuVijjakajohn"
+    qr_code_filename = "qrcode_image.png"
     
+    qr_code = qrcode.make(info)
+    qr_code.save(qr_code_filename)
     
+    return qr_code_filename
+
+def main():
+    window = tk.Tk()
+    window.title("QR CODE")
     
+    qr_code_filename = generate_qr_code()
     
+    image = Image.open(qr_code_filename)
+    photo = ImageTk.PhotoImage(image)
     
-    window.mainloop
+    image_label = tk.Label(window, image=photo)
+    image_label.pack()
+    
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
